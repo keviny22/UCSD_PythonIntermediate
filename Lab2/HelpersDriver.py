97dd1e616088c78df436084526cf723cf415c19b
kevin.young <kevin.young@servicenow.com>
2022-03-02 11:24:11 -0800

    completed lab 6

--- Lab2/HelpersDriver.py
+++ Lab2/HelpersDriver.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     print(inputInt("Please enter a number: ", 0, 100))
     print(inputFloat("Please enter a number: ", -10, 1000))
     print(inputString("Enter some text: ", 5, 10))
-    print(inputDate("Enter a date: "))
+    print(inputDate("Enter a date (yyyy-mm-dd): "))
--- main.sh
+++ main.sh
@@ -1,32 +1,53 @@
 #!/usr/bin/env bash
-echo Lab6
+echo ""
+echo ##########################################
+echo #             Lab6
+echo ##########################################
+echo ""
 cd Lab6
 python Lab6.py
 cd ..
 
-echo Lab5
+echo ""
+echo ##########################################
+echo #             Lab5
+echo ##########################################
+echo ""
 cd Lab5
 python Lab5.py
 cd ..
 
-
-echo Lab4
+echo ""
+echo ##########################################
+echo #             Lab4
+echo ##########################################
+echo ""
 cd Lab4
 echo "Skipping Lab4, its really long.. Run it manually if needed python PiIteratorDriver.py "
 #python PiIteratorDriver.py
 cd ..
 
-echo Lab3
+echo ""
+echo ##########################################
+echo #             Lab3
+echo ##########################################
+echo ""
 cd Lab3
 python StudentDriver.py
 cd ..
-
-echo Lab2
+echo ""
+echo ##########################################
+echo #             Lab2
+echo ##########################################
+echo ""
 cd Lab2
 python HelpersDriver.py
 cd ..
-
-echo Lab1
+echo ""
+echo ##########################################
+echo #             Lab1
+echo ##########################################
+echo ""
 cd Lab1
 python Lab1_Par1.py
 python Lab1_Par2.py
