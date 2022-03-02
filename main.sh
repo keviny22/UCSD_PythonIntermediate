9cff85354b4f0d752eabbde5c4f3ed41a89ff587
kevin.young <kevin.young@servicenow.com>
2022-03-02 11:18:21 -0800

    completed lab 6

--- main.sh
+++ main.sh
@@ -1,4 +1,34 @@
 #!/usr/bin/env bash
-
+echo Lab6
 cd Lab6
 python Lab6.py
+cd ..
+
+echo Lab5
+cd Lab5
+python Lab5.py
+cd ..
+
+
+echo Lab4
+cd Lab4
+echo "Skipping Lab4, its really long.. Run it manually if needed python PiIteratorDriver.py "
+#python PiIteratorDriver.py
+cd ..
+
+echo Lab3
+cd Lab3
+python StudentDriver.py
+cd ..
+
+echo Lab2
+cd Lab2
+python HelpersDriver.py
+cd ..
+
+echo Lab1
+cd Lab1
+python Lab1_Par1.py
+python Lab1_Par2.py
+python Lab1_Par3.py
+cd ..
